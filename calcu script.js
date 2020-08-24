7ad18a374f0bc9c00e6e6bfeda2a6ac1cc638607
ayishaamreenap <69688820+ayishaamreenap@users.noreply.github.com>
2020-08-24 17:15:55 +0530

    day 5

---
+++ calcu script.js
@@ -0,0 +1,14 @@
+function buttonClick(val)
+{
+    document.getElementById("screen").value=document.getElementById("screen").value+val;
+}
+function displayClear()
+{
+    document.getElementById("screen").value=""
+}
+function equalClick()
+{
+    var text=document.getElementById("screen").value
+    var result=eval(text)
+    document.getElementById("screen").value=result
+}
