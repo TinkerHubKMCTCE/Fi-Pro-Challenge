ae94beb4a55ef5ed169956af3bcf2d4ff5495f73
Aiswarya Mohan <69686283+AiswaryaMohan01@users.noreply.github.com>
2020-08-25 11:51:17 +0700

    tic tac toe
    My tic tac toe board

---
+++ tictac.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded',() => {
+     const squares=document.querySelectorAll('.grid div')
+     const playerDisplay=document.querySelector('#player')
+     let currentPlayer='playerX'
+
+
+     squares.forEach(square => {
+        square.addEventListener('click',clickOutcome)
+      } )
+      function clickOutcome(e){
+          const squareArray=Array.from(squares)
+          const index=squareArray.indexOf(e.target)
+          playerDisplay.innerHTML=currentPlayer
+          if(currentPlayer==='playerX'){
+              squares[index].classList.add('playerX')
+              currentPlayer='player0'
+          }
+          else{
+              squares[index].classList.add('player0')
+              currentPlayer='playerX'
+          }
+      }
+})
