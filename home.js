92754d62ab93a9de28b89998a7f3f52774351bf5
Roshni Joseph <69712886+Roshni-Joseph@users.noreply.github.com>
2020-09-03 12:36:35 +0530

    Responsive Portfolio
    HTML,CSS and JS are used.

--- home.js
+++ home.js
@@ -1,25 +1,17 @@
-document.addEventListener('DOMContentLoaded',() => {
-    const squares=document.querySelectorAll('.grid div')
-    const playerDisplay=document.querySelector('#player')
-    let currentPlayer='playerX'
-    squares.forEach(square => {
-        square.addEventListener('click',clickOutcome)
-    })
-    function clickOutcome(e){
-        const squareArray=Array.from(squares)
-        const index=squareArray.indexOf(e.target)
-        playerDisplay.innerHTML=currentPlayer
-        if(currentPlayer==='playerX'){
-            squares[index].classList.add('playerX')
-            currentPlayer='player0'
-        }else{
-            squares[index].classList.add('player0')
-            currentPlayer='playerX'
-        }
-    }
-
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '80px',
+    duration: 2000,
+    reset: true
 })
-
-
-
-
+sr.reveal('.hey',{})
+sr.reveal('.social-icons',{delay:500,origin:'left'})
+sr.reveal('.description',{})
+sr.reveal('.roshimg',{delay:500,origin:'right'})
+sr.reveal('.python-mg',{delay:100,})
+sr.reveal('.html-mg',{delay:200,})
+sr.reveal('.css-mg',{delay:300,})
+sr.reveal('.java-mg',{delay:400,})
+sr.reveal('.js-mg',{delay:500,})
+sr.reveal('.c-mg',{delay:600,})
+sr.reveal('.ps-mg',{delay:700,})
